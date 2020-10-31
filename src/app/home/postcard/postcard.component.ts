9fd152c86e70b6942007890b69cd1b7bcaf3fd68
ABHINAV TOMAR <atomar.cse18@chitkarauniversity.edu.in>
2020-10-31 20:43:59 +0530

    Post Card Init

---
+++ src/app/home/postcard/postcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postcard',
+  templateUrl: './postcard.component.html',
+  styleUrls: ['./postcard.component.sass']
+})
+export class PostcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
